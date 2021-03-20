64b916143901b01ce313897e4474349b035ede5c
cam12-a <camara.alseny@yandex.ru>
2021-03-20 18:49:21 +0300

    end left menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,19 @@
 
-function menu() {
+$(document).ready(function() {
+	$('.menu-salat').click(function(){
+		$('.salat').slideToggle("slow");
+	});
+	$('.menu-bake').click(function(){
+		$('.bake').slideToggle("slow");
+	});
+
+	$('.menu-salat').animate({transform: 'translateY(-20px) rotate(-180deg)'});
+
+	function menu() {
 	document.getElementById("rightHeader").style.display='block';
 }
 function closeMenu() {
 	document.getElementById("rightHeader").style.display='none';
 }
+
+});	
